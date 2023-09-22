390fe811c0166202021af923f21186bb9a9bc6fe
AthanThom <konialis789@gmail.com>
2023-09-22 15:43:59 +0300

    regex validation phones

--- src/dtos/receivers/receiver-sms.dto.ts
+++ src/dtos/receivers/receiver-sms.dto.ts
@@ -1,13 +1,22 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsArray, IsPhoneNumber } from 'class-validator';
+import { Matches } from 'class-validator';
 
 export class SmsReceiverDto {
-  @IsArray()
-  @IsPhoneNumber()
+  @Matches(/^\+\d{1,3}\.\d{9,11}$/, { message: 'Invalid Phone Number' })
   @ApiProperty({
-    type: [String],
-    description: 'An array of phone numbers for the receivers',
-    example: ['+1234567890', '+9876543210'],
+    type: String,
+    description: `A phone number for the receiver. It must match the following regex pattern:
+      ^\\+\\d{1,3}\\.\\d{9,11}$
+      Explanation of the regex pattern:
+      ^ asserts position at start of a line
+      \\+ matches the character + literally (case sensitive)
+      \\d matches a digit (equivalent to [0-9])
+      {1,3} matches the previous token between 1 and 3 times, as many times as possible (greedy)
+      \\. matches the character . literally (case sensitive)
+      \\d matches a digit (equivalent to [0-9])
+      {9,11} matches the previous token between 9 and 11 times, as many times as possible (greedy)
+      $ asserts position at the end of a line`,
+    example: '+XX.XXXXXXXXXX',
   })
   receiver_phone: string;
 }
--- src/dtos/senders/sender-sms.dto.ts
+++ src/dtos/senders/sender-sms.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, IsPhoneNumber, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, Matches } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class SmsSenderDto {
@@ -12,11 +12,21 @@ export class SmsSenderDto {
   message_id: string;
 
   @IsNotEmpty()
-  @IsPhoneNumber()
+  @Matches(/^\+\d{1,3}\.\d{9,11}$/)
   @ApiProperty({
     type: String,
-    description: 'The sender phone number',
-    example: '+1234567890',
+    description: `The sender phone number. It must match the following regex pattern:
+    ^\\+\\d{1,3}\\.\\d{9,11}$
+    Explanation of the regex pattern:
+    ^ asserts position at start of a line
+    \\+ matches the character + literally (case sensitive)
+    \\d matches a digit (equivalent to [0-9])
+    {1,3} matches the previous token between 1 and 3 times, as many times as possible (greedy)
+    \\. matches the character . literally (case sensitive)
+    \\d matches a digit (equivalent to [0-9])
+    {9,11} matches the previous token between 9 and 11 times, as many times as possible (greedy)
+    $ asserts position at the end of a line`,
+    example: '+XX.XXXXXXXXXX',
   })
   sender_phone: string;
 
